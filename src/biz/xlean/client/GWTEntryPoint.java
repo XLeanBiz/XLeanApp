c5914c9c5d4fcedcc00da4a2382a719a7f77febb
Owner <Owner@10.0.0.78>
2012-02-23 20:15:16 -0500

    about

--- src/biz/xlean/client/GWTEntryPoint.java
+++ src/biz/xlean/client/GWTEntryPoint.java
@@ -3,12 +3,15 @@ package biz.xlean.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.startupstages.client.blog.BlogPanel;
+import com.startupstages.client.blog.steps.Ideation;
 import com.startupstages.client.model.TopicsMockup;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GWTEntryPoint implements EntryPoint {
+
+	public static AboutXLean aboutXlean = new AboutXLean();
 
 	/**
 	 * This is the entry point method.
@@ -21,5 +24,10 @@ public class GWTEntryPoint implements EntryPoint {
 
 		Home.vpMain.clear();
 		Home.vpMain.add(new BlogPanel());
+
+		Ideation.initializeIdeationPanel();
+
+		aboutXlean.center();
+		aboutXlean.show();
 	}
 }
